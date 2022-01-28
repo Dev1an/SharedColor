8d89c3136546faf8a4a43b99c4885931c59593a6
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2022-01-28 10:37:14 +0100

    Initial Commit

---
+++ Shared/TheoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheoPlayApp.swift
+//  Shared
+//
+//  Created by damiaan on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
